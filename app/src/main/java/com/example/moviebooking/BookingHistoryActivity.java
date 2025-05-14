4b86dd3d58f4bc9d63a5df33f4a0776eb2058be7
curyhao123 <ccuurryy2005@gmail.com>
2025-05-14 23:07:54 +0700

    bookinghistory+status

--- app/src/main/java/com/example/moviebooking/BookingHistoryActivity.java
+++ app/src/main/java/com/example/moviebooking/BookingHistoryActivity.java
@@ -1,24 +1,61 @@
 package com.example.moviebooking;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.moviebooking.R;
+import com.example.moviebooking.data.FireBaseManager;
+import com.example.moviebooking.dto.*;
+
+import java.util.List;
+import com.example.moviebooking.home.TicketListAdapter;
+
 
 public class BookingHistoryActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private UserInfo userInfo = null;
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_booking_history);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(com.example.moviebooking.R.layout.activity_booking_history);
+        userInfo = (UserInfo) getIntent().getSerializableExtra("userinfoIntent");
+        TextView username = findViewById(R.id.tv_user_name);
+        username.setText(userInfo.getName());
+
+        Log.d("BookingHistoryActivity", "onCreate: " + userInfo.getUsername());
+
+        fetchDataForTicketHistory(userInfo);
+    }
+
+    private void fetchDataForTicketHistory(UserInfo userInfo) {
+        FireBaseManager firebaseManager = FireBaseManager.getInstance();
+        firebaseManager.fetchUserTickets(userInfo, new FireBaseManager.OnBookedSeatsLoadedListener() {
+            @Override
+            public void onBookedSeatsLoaded(List<Ticket> tickets) {
+                if (tickets != null) {
+                    tickets.sort((o1, o2) -> o2.getBookedTime().compareTo(o1.getBookedTime()));
+                    Log.d("BookingHistoryActivity", "onBookedSeatsLoaded: " + tickets.size());
+                    initRecyclerView(tickets);
+                }
+            }
+            @Override
+            public void onBookedSeatsError(String errorMessage) {
+            }
         });
+    }
+
+    private void initRecyclerView(List<Ticket> tickets) {
+        RecyclerView historyTicketView = findViewById(R.id.rcv_ticket_history);
+        TicketListAdapter adapter = new TicketListAdapter(this, userInfo, tickets);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        historyTicketView.setLayoutManager(linearLayoutManager);
+
+        historyTicketView.setAdapter(adapter);
     }
 }
